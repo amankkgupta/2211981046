528bf7c77f2fd43e11b2af46d0735ac562c1daac
amankkgupta <amangupta65734@gmail.com>
2025-04-15 12:09:50 +0530

    first

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+const express= require("express");
+const app= express();
+const dotenv= require('dotenv');
+const cors= require('cors');
+dotenv.config();
+app.use(cors());
+
+app.get("/",(req, res)=>{
+    res.send("Welcome, I am running");
+})
+app.get("/users", (req, res)=>{
+
+})
+
+app.listen(process.env.PORT,()=> console.log("server started..."));
